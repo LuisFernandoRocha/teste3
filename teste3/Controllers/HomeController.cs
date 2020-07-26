953c5e8ef3bf33efa0854d88882c30ce71974e73
Luis Fernando Rocha <Fernando23dompa2hotmail.com>
2020-07-26 12:27:12 -0300

    teste3

---
+++ teste3/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using teste3.Models;
+
+namespace teste3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Novo(int Codigo)
+        {
+            Moeda objConta = new Moeda();
+            int cod = objConta.Novo(Codigo);         
+            return Json(new { sucesso = true, mensagem = cod });
+        }
+
+        public IActionResult Salvar(int codigo,string descricao,string abreviatura, double cotacao)
+        {
+            Moeda objConta = new Moeda();
+            int cod = objConta.salvar(codigo,descricao,abreviatura,cotacao);
+
+            if (cod == 0)
+            {
+                return Json(new { sucesso = true });
+            }
+            else
+            {
+                return Json(new { sucesso = false });
+            }
+        }
+
+        public IActionResult Excluir(int codigo)
+        {
+            Moeda objConta = new Moeda();
+             int cod = objConta.Excluir(codigo);
+            if (cod==0)
+            {
+                return Json(new { sucesso = false });
+            }
+            else
+            {
+                return Json(new { sucesso = true });
+            }
+   
+       }
+    }
+}
---
+++ teste3/Models/Moeda.cs
@@ -0,0 +1,64 @@
+﻿using MyFinance.Util;
+using System.Data;
+using Xunit;
+using System.ComponentModel.DataAnnotations;
+
+namespace teste3.Models
+{
+    public class Moeda
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Abreviatura { get; set; }
+        public double Cotacao { get; set; }
+        public Moeda(){}
+        public Moeda(int codigo, string descricao, string abreviatura, double cotacao)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+            Abreviatura = abreviatura;
+            Cotacao = cotacao;
+        }
+
+        public int salvar(int codigo, string descricao, string abreviatura, double cotacao)
+        {
+            try
+            {
+                var sql = $"SELECT * FROM db_moedas where codigo = '{codigo}'";
+                DAL objDAL = new DAL();
+                DataTable dt = objDAL.RetDataTable(sql);
+                int retorno = int.Parse(dt.Rows[0]["codigo"].ToString());
+                return retorno;
+            }
+            catch
+            {
+                new DAL().ExecutarComandoSQL("insert into db_moedas values ('" + codigo + "','" + descricao + "','" + abreviatura + "','" + cotacao + "')");
+                return 0;
+            }
+        }
+
+        public int Novo(int codigo)
+        {    
+            var sql = $" SELECT (max(codigo) + 1) as codigo FROM db_moedas;";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);    
+            return int.Parse(dt.Rows[0]["codigo"].ToString());
+        }
+        public int Excluir(int codigo)
+        {
+            try
+            {
+                var sql = $"SELECT * FROM db_moedas where codigo = '{codigo}'";
+                DAL objDAL = new DAL();
+                DataTable dt = objDAL.RetDataTable(sql);
+                int retorno = int.Parse(dt.Rows[0]["codigo"].ToString());
+                new DAL().ExecutarComandoSQL("Delete from db_moedas where codigo= " + codigo);
+                return retorno;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
